3c8b31ff67fa10b96d65966909d5d4d7ceb408bf
Eric Pulick <pulick@wisc.edu>
2023-04-19 19:27:08 -0500

    parameter testing

--- active/cluster_driver_alt.py
+++ active/cluster_driver_alt.py
@@ -50,11 +50,11 @@ if __name__ == "__main__":
     args.update({'RULE_FILE_PATH' : rule_file_path})
     # Experiment updates for REINFORCE
     args.update({"LEARNER":"REINFORCE"})
-    args.update({"LR":0.0008})
+    args.update({"LR":0.0012})
     args.update({"ACTIVATION":"LeakyReLU"})
     args.update({"OPTIMIZER":"ADAM"})
-    args.update({"TRAIN_EPISODES":10000})
-    args.update({"HIDDEN_SIZES":[500]})
+    args.update({"TRAIN_EPISODES":12000})
+    args.update({"HIDDEN_SIZES":[600]})
     #print(args)
     run_experiment(args)
 
